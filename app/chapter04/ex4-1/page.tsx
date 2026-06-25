fa8e9d04b793135e7b1212bbb7ee06e139c2dc17
H-Akaogi <hazumi.akaogi@wemex.com>
2026-06-25 12:32:29 +0900

    演習 4-1 四則計算に変更

--- app/chapter04/ex4-1/page.tsx
+++ app/chapter04/ex4-1/page.tsx
@@ -7,30 +7,58 @@
 import ActionButton from "@/components/chapter04/ex4-1/button/ActionButton";
 import NumberInput from "@/components/chapter04/ex4-1/input/NumberInput";
 import { useState } from "react";
+// 追加
+import SelectInput from "@/components/chapter04/list4/select/SelectInput";
 
 export default function CalculatorPage() {
     // 1. 入力値を記憶するState（初期値を 0 に設定）
     const [num1, setNum1] = useState(0);
     const [num2, setNum2] = useState(0);
 
+    // 2. 計算方法を記憶するState（初期値を「選択してください」に設定
+    const [calc, setCalc] = useState("選択してください");
+    const CalcOptions = [
+        { value: "+", label: "+ (足し算)" },
+        { value: "-", label: "- (引き算)" },
+        { value: "×", label: "× (掛け算)" },
+        { value: "÷", label: "÷ (割り算)" }
+    ]
     // 2. 計算結果を記憶するState（初期値を 0 に設定）
     const [result, setResult] = useState(0);
 
     // 計算ボタンが押された時の処理
     const handleCalculate = () => {
+        let sum = 0;
+        if (calc === "+") {
+            sum = num1 + num2;
+        }
+        else if (calc === "-") {
+            sum = num1 - num2;
+        }
+        else if (calc === "×") {
+            sum = num1 * num2;
+        }
+        else if (calc === "÷") {
+            sum = num1 / num2;
+        }
         // 子部品(NumberInput)が確実に数値で渡してくれるため、シンプルに足し算できる
-        const sum = num1 + num2;
+        //const sum = num1 + num2;
         setResult(sum);
     };
 
     return (
         <main className="p-4 max-w-sm mx-auto mt-10 border rounded shadow-sm bg-white">
-            <h1 className="text-xl font-bold mb-4 border-b pb-2">加算計算機</h1>
+            <h1 className="text-xl font-bold mb-4 border-b pb-2">計算機</h1>
 
             {/* 数値入力専用コンポーネントを2つ配置 */}
             <NumberInput label="数値1" value={num1} onChange={(val) => setNum1(val)} />
             <NumberInput label="数値2" value={num2} onChange={(val) => setNum2(val)} />
-
+            <SelectInput
+                label="計算方法"
+                options={CalcOptions}
+                value={calc}
+                onChange={(newValue) => setCalc(newValue)}
+            />
             <div className="mt-4 mb-6">
                 <ActionButton label="計算する" onClick={handleCalculate} isRounded={true} />
             </div>
