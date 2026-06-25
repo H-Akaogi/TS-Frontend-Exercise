a216925ccee11af50ee191154b47bb30b2c0b7ea
H-Akaogi <hazumi.akaogi@wemex.com>
2026-06-25 14:59:34 +0900

    リスト4-6 CheckboxGroupInput(チェックボックス)コンポーネントの実装

--- app/layout/basic.tsx
+++ app/layout/basic.tsx
@@ -53,6 +53,10 @@ export default function RootLayout({
                             リスト4-5 RadioGroupInput(ラジオボタン)コンポーネントの実装
                             */}
                             <a href="/chapter04/list4/radio" className="hover:underline">ラジオボタン</a>
+                            {/* 
+                            リスト4-6 CheckboxGroupInput(チェックボックス)コンポーネントの実装
+                            */}
+                            <a href="/chapter04/list4/check" className="hover:underline">チェックボックス</a>
                             {/* 演習 4-1 計算機を作成する(加算機能のみ) */}
                             <a href="/chapter04/ex4-1" className="hover:underline">計算機(ラジオボタン)</a>
                             {/* 演習 4-2 計算の種類をプルダウンで選択可能にする */}
