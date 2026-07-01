8b2978204a33d45aa41357964914a38dc3e8ba6c
H-Akaogi <hazumi.akaogi@wemex.com>
2026-07-01 16:00:02 +0900

    編集

--- infrastructures/MockProductRepository.ts
+++ infrastructures/MockProductRepository.ts
@@ -1,6 +1,7 @@
 import { injectable } from "inversify";
 import { IProductRepository } from "../interfaces/IProductRepository";
 import { Product } from "../models/Product";
+import { ProductRegistration } from "@/models/ProductRegistration";
 /**
  * 演習 6-2 データアクセスとサービスを実装する
  * 商品リポジトリの実装(モック)
@@ -49,5 +50,11 @@ export class MockProductRepository implements IProductRepository {
         );
         // asyncメソッドなので、自動的にPromiseでラップされて返却される
         return filteredProducts;
+    }
+    public async existsByName(name: string): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+    public async register(product: ProductRegistration): Promise<Product> {
+        throw new Error("Method not implemented.");
     }
 }
